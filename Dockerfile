3608bf3d831352a8cb703c39b382e7e299fcbc08
Amaan Ansari <m.amaan02@gmail.com>
2023-10-23 21:11:19 +0530

    Initial Commit from local

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+ARG IMAGE_VARIANT=slim-buster
+ARG OPENJDK_VERSION=8
+ARG PYTHON_VERSION=3.9.8
+
+FROM python:${PYTHON_VERSION}-${IMAGE_VARIANT} AS py3
+FROM openjdk:${OPENJDK_VERSION}-${IMAGE_VARIANT}
+
+COPY --from=py3 / /
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r req.txt
+
+CMD ["streamlit", "run", "page.py"]
---
+++ page.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+from pyspark.sql import SparkSession, Row
+from pyspark.ml import PipelineModel
+
+spark = SparkSession.builder.appName("bigData").getOrCreate()
+model = PipelineModel.load('files/airport-shiz/')
+
+st.title("DelayDecoded")
+
+df = pd.read_csv("airport-data.csv")
+
+st.sidebar.title("Input Features")
+
+st.sidebar.markdown("Select the features that you want to input.")
+
+airline = st.sidebar.selectbox("Airline", df["Airline"].unique())
+origin = st.sidebar.selectbox("Origin", df["Origin"].unique())
+dest = st.sidebar.selectbox("Destination", df["Dest"].unique())
+distance = st.sidebar.number_input("Distance", value=df['Distance'].mean())
+year = st.sidebar.number_input("Year", value=int(df['Year'].median()))
+quarter = st.sidebar.number_input("Quarter", value=int(df['Quarter'].median()))
+month = st.sidebar.number_input("Month", value=int(df['Month'].median()))
+day_of_month = st.sidebar.number_input("Day of Month", value=int(df['DayofMonth'].median()))
+day_of_week = st.sidebar.number_input("Day of Week", value=int(df['DayOfWeek'].median()))
+marketing_airline_network = st.sidebar.selectbox("Marketing Airline Network", df["Marketing_Airline_Network"].unique())
+operated_or_branded_code_share_partners = st.sidebar.selectbox("Operated or Branded Code Share Partners", df["Operated_or_Branded_Code_Share_Partners"].unique())
+iata_code_marketing_airline = st.sidebar.selectbox("IATA Code Marketing Airline", df["IATA_Code_Marketing_Airline"].unique())
+operating_airline = st.sidebar.selectbox("Operating Airline", df["Operating_Airline"].unique())
+iata_code_operating_airline = st.sidebar.selectbox("IATA Code Operating Airline", df["IATA_Code_Operating_Airline"].unique())
+crs_arr_time = st.sidebar.number_input("CRS Arrival Time", value=df['CRSArrTime'].median())
+crs_dep_time = st.sidebar.number_input("CRS Departure Time", value=df['CRSDepTime'].median())
+crs_elapsed_time = st.sidebar.number_input("CRS Elapsed Time", value=df['CRSElapsedTime'].median())
+
+
+if st.button("Predict"):
+    st.write("Predicted Delay: 10")
+    model = PipelineModel.load('files/airport-index/')
+
