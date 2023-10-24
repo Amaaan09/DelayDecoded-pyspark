abc2347db79fa01276cc8f6ee65f67dfae84ff8f
Amaan Ansari <m.amaan02@gmail.com>
2023-10-24 12:05:59 +0530

    Trying Stremlit

--- trial.py
+++ trial.py
@@ -1,4 +1,5 @@
 from pyspark.ml import PipelineModel
+import streamlit as st
 
 model = PipelineModel.load('files/airport-shiz/')
 print("Model loaded")
@@ -8,3 +9,7 @@ spark = SparkSession.builder.appName("bigData").getOrCreate()
 
 df = spark.read.csv("airport-data.csv", header=True, inferSchema=True)
 print("Data loaded")
+
+st.title("Airport Prediction shiz")
+
+st.write("This is a simple web app to predict the airport based on the input data")
